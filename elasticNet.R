dd001d3b4547eab5ee7143c4523d24f091aace5f
thistleknot <rstudio@rstudio.localhost.localdomain>
2020-09-29 02:46:16 +0000

    found best lowest error model method using bestregsubset and cv

--- elasticNet.R
+++ elasticNet.R
@@ -11,6 +11,9 @@ library(psych)   # for function tr() to compute trace of a matrix
 library(parallel)
 library(MLmetrics)
 library(pls)
+library(caret)
+library(leaps)
+library(tidyverse)
 
 preData <- read.csv(file="states.csv", header=T)
 data <- preData[,c(-1)]
@@ -43,7 +46,62 @@ finalResults <- round(coef(pls.model)[,1,1],2)
 
 finalNames <- names(finalResults[abs(finalResults)>0])
 
-formula = paste(colnames(Y)~finalNames)
+f <- as.formula(
+  paste(colnames(Y), 
+        paste(finalNames, collapse = " + "), 
+        sep = " ~ "))
+
+model <- lm(f,data)
+
+summary(model)
+
+#http://www.sthda.com/english/articles/37-model-selection-essentials-in-r/155-best-subsets-regression-essentials-in-r/#:~:text=The%20best%20subsets%20regression%20is,best%20subsets%20regression%20using%20R.
+# id: model id
+# object: regsubsets object
+# data: data used to fit regsubsets
+# outcome: outcome variable
+get_model_formula <- function(id, object, outcome){
+  # get models data
+  models <- summary(object)$which[id,-1]
+  # Get outcome variable
+  #form <- as.formula(object$call[[2]])
+  #outcome <- all.vars(form)[1]
+  # Get model predictors
+  predictors <- names(which(models == TRUE))
+  predictors <- paste(predictors, collapse = "+")
+  # Build model formula
+  as.formula(paste0(outcome, "~", predictors))
+}
+
+models <- regsubsets(Poverty~., data = data, nvmax = 5)
+res.sum <- summary(models)
+data.frame(
+  Adj.R2 = which.max(res.sum$adjr2),
+  CP = which.min(res.sum$cp),
+  BIC = which.min(res.sum$bic)
+)
+get_cv_error <- function(model.formula, data){
+  set.seed(1)
+  train.control <- trainControl(method = "cv", number = 5)
+  cv <- train(model.formula, data = data, method = "lm",
+              trControl = train.control)
+  cv$results$RMSE
+}
+# Compute cross-validation error
+model.ids <- 1:5
+
+#get_model_formula(3, models, "Poverty")
+
+model.ids <- 1:5
+cv.errors <-  map(model.ids, get_model_formula, models, "Poverty") %>%
+  map(get_cv_error, data = data) %>%
+  unlist()
+
+cv.errors
+
+coef(models, which.min(cv.errors))
+
+finalNames <- names(coef(models, which.min(cv.errors)))[-1]
 
 f <- as.formula(
   paste(colnames(Y), 
