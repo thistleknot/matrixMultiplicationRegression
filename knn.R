2b9b734d522ce02a0fa27a12bc34c32e5a647369
thistleknot <rstudio@rstudio.localhost.localdomain>
2020-10-04 22:05:38 +0000

    looks like it's working correctly.  Need to check on my low sdev

--- knn.R
+++ knn.R
@@ -2,7 +2,7 @@ source("functions.R")
 
 #knn.reg.bestK(x_training, x_test, y_training, y_test)
 
-set.train <- set.train2
+set.train2 <- set.train
 
 dataset_knn <- back_step_partial_correlation_knn(set.train2)
 
@@ -10,7 +10,7 @@ dataset_reduced <- rmse_backstep_knn(dataset_knn)
 
 knn_predictors <- read.csv(text=paste (dataset_reduced[[2]][[1]], collapse=","),header = FALSE)
 
-set.train <- set.train2[,c(unlist(knn_predictors))]
+set.train2 <- set.train2[,c(unlist(knn_predictors))]
 
 corPlot(PCOR(set.train2))
 knn_model <- knn.reg.bestK(set.train2)
@@ -22,8 +22,9 @@ yhat = knn.reg(set.train[,-1,drop=FALSE], test=set.test[,colnames(set.train)][,-
 #yhat$R2Pred
 
 #predict on self data with best knn
-yhatFull = knn.reg(set.final[,colnames(set.train)][,-1,drop=FALSE], test=set.final[,colnames(set.train)][,-1,drop=FALSE], unlist(set.final[,colnames(set.train)][,1,drop=FALSE]), knn_model$k.opt)
+#yhatFull = knn.reg(set.final[,colnames(set.train)][,-1,drop=FALSE], test=set.final[,colnames(set.train)][,-1,drop=FALSE], unlist(set.final[,colnames(set.train)][,1,drop=FALSE]), knn_model$k.opt)
 RMSE(yhat$pred,unlist(set.test[,1]))
+MAPE(yhat$pred,unlist(set.test[,1]))
 plot(yhat$pred,unlist(set.test[,1]))
 RMSE(yhatFull$pred,unlist(set.final[,1]))
 
