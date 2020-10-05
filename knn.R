73acde2f4644464c016f17a3daf5f8eaf4d897f0
thistleknot <rstudio@rstudio.localhost.localdomain>
2020-10-05 00:56:29 +0000

    better knn

--- knn.R
+++ knn.R
@@ -2,11 +2,24 @@ source("functions.R")
 
 #knn.reg.bestK(x_training, x_test, y_training, y_test)
 
+#normalization (not being used)
+if(normalizeResponse=="Y")
+{
+  #trainParam <- caret::preProcess(as.matrix(set.train),method=c("BoxCox", "center", "scale"))
+  trainParam <- caret::preProcess(as.matrix(set.train),method=c("center", "scale"))
+  
+  set.train <- predict(trainParam, data2)
+  
+  set.test <- predict(trainParam, data2)
+}
+
+
 set.train2 <- set.train
 
 dataset_knn <- back_step_partial_correlation_knn(set.train2)
-
 dataset_reduced <- rmse_backstep_knn(dataset_knn)
+
+#dataset_reduced <- rmse_backstep_knn(set.train2)
 knn_predictors <- read.csv(text=paste (dataset_reduced[[2]][[1]], collapse=","),header = FALSE)
 
 #knn_predictors <- colnames(dataset_knn)
@@ -17,21 +30,29 @@ corPlot(PCOR(set.train2))
 knn_model <- knn.reg.bestK(set.train2)
 
 #knn_model$k.opt
-
+tested1 <- set.test[,1]
+tested2 <- rbind(set.train,set.test)[,1]
 #predict on test data
-yhat = knn.reg(set.train[,-1,drop=FALSE], test=set.test[,colnames(set.train)][,-1,drop=FALSE], unlist(set.train[,1,drop=FALSE]), knn_model$k.opt)
-yhatFull = knn.reg(rbind(set.train,set.test)[,colnames(set.train2)][,-1,drop=FALSE], test=rbind(set.train,set.test)[,colnames(set.train2)][,-1,drop=FALSE], unlist(rbind(set.train,set.test)[,colnames(set.train2)][,1,drop=FALSE]), knn_model$k.opt)
+predictions1 = knn.reg(set.train[,-1,drop=FALSE], test=set.test[,colnames(set.train)][,-1,drop=FALSE], unlist(set.train[,1,drop=FALSE]), knn_model$k.opt)
+predictions2 = knn.reg(rbind(set.train,set.test)[,colnames(set.train2)][,-1,drop=FALSE], test=rbind(set.train,set.test)[,colnames(set.train2)][,-1,drop=FALSE], unlist(rbind(set.train,set.test)[,colnames(set.train2)][,1,drop=FALSE]), knn_model$k.opt)
 #yhat$R2Pred
 
+if(normalizeResponse=="Y")
+{
+  predictions1 <- (predictions1$pred * trainParam$std[1]) + trainParam$mean[1]
+  predictions2 <- (predictions2$pred * trainParam$std[1]) + trainParam$mean[1]
+  tested1 <- (tested1 * trainParam$std[1]) + trainParam$mean[1]
+  tested2 <- (tested2 * trainParam$std[1]) + trainParam$mean[1]
+}
 
 #predict on self data with best knn
-RMSE(yhat$pred,unlist(set.test[,1]))
-MAPE(yhat$pred,unlist(set.test[,1]))
-plot(yhat$pred,unlist(set.test[,1]))
+RMSE(predictions1,unlist(tested1))
+MAPE(predictions1,unlist(tested1))
+plot(predictions1,unlist(tested1))
 
-RMSE(yhatFull$pred,unlist(rbind(set.train,set.test)[,1]))
-MAPE(yhatFull$pred,unlist(rbind(set.train,set.test)[,1]))
-plot(yhatFull$pred,unlist(rbind(set.train,set.test)[,1]))
+RMSE(predictions2,unlist(tested2))
+MAPE(predictions2,unlist(tested2))
+plot(predictions2,unlist(tested2))
 
 #resid <- unlist(set.final[,1])-yhatFull$pred
 
