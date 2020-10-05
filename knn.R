fc963e4446669777b752fc416efd730086da0650
thistleknot <rstudio@rstudio.localhost.localdomain>
2020-10-05 00:18:13 +0000

    knn is too punishing. I'm not sure if I should be doing the partial correlation check

--- knn.R
+++ knn.R
@@ -7,10 +7,11 @@ set.train2 <- set.train
 dataset_knn <- back_step_partial_correlation_knn(set.train2)
 
 dataset_reduced <- rmse_backstep_knn(dataset_knn)
-
 knn_predictors <- read.csv(text=paste (dataset_reduced[[2]][[1]], collapse=","),header = FALSE)
 
-set.train2 <- set.train2[,c(unlist(knn_predictors))]
+#knn_predictors <- colnames(dataset_knn)
+
+set.train2 <- set.train[,c(unlist(knn_predictors))]
 
 corPlot(PCOR(set.train2))
 knn_model <- knn.reg.bestK(set.train2)
@@ -19,21 +20,25 @@ knn_model <- knn.reg.bestK(set.train2)
 
 #predict on test data
 yhat = knn.reg(set.train[,-1,drop=FALSE], test=set.test[,colnames(set.train)][,-1,drop=FALSE], unlist(set.train[,1,drop=FALSE]), knn_model$k.opt)
+yhatFull = knn.reg(rbind(set.train,set.test)[,colnames(set.train2)][,-1,drop=FALSE], test=rbind(set.train,set.test)[,colnames(set.train2)][,-1,drop=FALSE], unlist(rbind(set.train,set.test)[,colnames(set.train2)][,1,drop=FALSE]), knn_model$k.opt)
 #yhat$R2Pred
 
+
 #predict on self data with best knn
-#yhatFull = knn.reg(set.final[,colnames(set.train)][,-1,drop=FALSE], test=set.final[,colnames(set.train)][,-1,drop=FALSE], unlist(set.final[,colnames(set.train)][,1,drop=FALSE]), knn_model$k.opt)
 RMSE(yhat$pred,unlist(set.test[,1]))
 MAPE(yhat$pred,unlist(set.test[,1]))
 plot(yhat$pred,unlist(set.test[,1]))
-RMSE(yhatFull$pred,unlist(set.final[,1]))
 
-resid <- unlist(set.final[,1])-yhatFull$pred
+RMSE(yhatFull$pred,unlist(rbind(set.train,set.test)[,1]))
+MAPE(yhatFull$pred,unlist(rbind(set.train,set.test)[,1]))
+plot(yhatFull$pred,unlist(rbind(set.train,set.test)[,1]))
 
-#library(dbscan)
-kNNdistplot(set.train[,colnames(set.train)], k=knn_model$k.opt)
+#resid <- unlist(set.final[,1])-yhatFull$pred
 
-eps <- median(kNNdist(set.train[,colnames(set.train)], k=knn_model$k.opt))
+library(dbscan)
+kNNdistplot(rbind(set.train,set.test)[,colnames(set.train2)], k=knn_model$k.opt)
 
-cl <- dbscan(set.final[,colnames(set.train)], eps = eps, minPts = knn_model$k.opt)
-pairs(set.final[,colnames(set.train)], col = cl$cluster+1L)
+eps <- median(kNNdist(rbind(set.train,set.test)[,colnames(set.train2)], k=knn_model$k.opt))
+
+cl <- dbscan(rbind(set.train,set.test)[,colnames(set.train2)], eps = eps, minPts = knn_model$k.opt)
+pairs(rbind(set.train,set.test)[,colnames(set.train2)], col = cl$cluster+1L)
