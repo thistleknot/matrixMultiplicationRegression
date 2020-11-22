3b821f04ce6082964fa47198061c339dd68d66f9
thistleknot <rstudio@rstudio.localhost.localdomain>
2020-11-22 21:11:59 +0000

    confirmed enet and glm are the same

--- glnnet Elastic Net.R
+++ glnnet Elastic Net.R
@@ -80,10 +80,10 @@ plot(enet)
 #coef(enet$finalModel,enet$bestTune[,1])
 
 #really bad values
-#model <- glmnet(data.matrix(data2[,-1,drop=FALSE]), as.matrix(data2[,1,drop=FALSE]), alpha = enet$finalModel$tuneValue$fraction, lambda=enet$finalModel$lambda, standardize = FALSE, family = "gaussian", type.measure="deviance")
+model_glm <- glmnet(data.matrix(set.train[,-1,drop=FALSE]), as.matrix(set.train[,1,drop=FALSE]), alpha = enet$finalModel$tuneValue$fraction, lambda=enet$finalModel$lambda, standardize = FALSE, family = "gaussian", type.measure="deviance")
 #predicts <- predict.glmnet(model,data.matrix(set.test[,-1,drop=FALSE]),alpha = enet$finalModel$tuneValue$fraction, lambda=enet$finalModel$lambda)
 
-best_caret <- train(f, data = data2,method = "leapBackward", trControl = train.control)
+best_caret <- train(f, data = set.train,method = "leapBackward", trControl = train.control)
 
 best_caret$finalModel
 
@@ -96,7 +96,7 @@ model_enet <- glmnet(data.matrix(set.train[,-1,drop=FALSE]), as.matrix(set.train
 
 plot(coef(model_enet))
 
-predict_glm <- predict.glmnet(model,data.matrix(set.test[,-1,drop=FALSE]),lambda=model$finalModel$tuneValue$lambda,s0=model$finalModel$tuneValue$fraction)
+predict_glm <- predict.glmnet(model_glm,data.matrix(set.test[,-1,drop=FALSE]),lambda=model_enet$finalModel$tuneValue$lambda,s0=model_enet$finalModel$tuneValue$fraction)
 
 model_xg <- glmnet(data.matrix(set.train[,-1,drop=FALSE]), as.matrix(set.train[,1,drop=FALSE]), alpha = xg$finalModel$tuneValue$alpha, lambda=xg$finalModel$lambda, standardize = FALSE, family = "gaussian", type.measure="deviance")
 
@@ -105,7 +105,7 @@ plot(coef(model_xg)[,ncol(coef(model_xg))])
 predict_xg <- predict.glmnet(model_xg,data.matrix(set.test[,-1,drop=FALSE]), alpha = xg$finalModel$tuneValue$alpha, lambda=xg$finalModel$lambda)
 predict_xg <- predict_xg[,ncol(predict_xg)]
 
-#predict_enet <- predict(model_enet,set.test[,-1])
+predict_enet <- predict(model_enet,as.matrix(set.test[,-1]))
 
 #same as best parm's
 #predict_xg <- predict(model_xg,set.test[,-1])
@@ -115,7 +115,7 @@ rmse((set.test[,1]* trainParam$std[1]) + trainParam$mean[1],(predict_glm* trainP
 rmse((set.test[,1]* trainParam$std[1]) + trainParam$mean[1],(predict_xg* trainParam$std[1]) + trainParam$mean[1])
 rmse((set.test[,1]* trainParam$std[1]) + trainParam$mean[1],(predict_best_caret* trainParam$std[1]) + trainParam$mean[1])
 
-
+#exact same
 plot((set.test[,1]* trainParam$std[1]) + trainParam$mean[1],(predict_enet* trainParam$std[1]) + trainParam$mean[1])
 #***
 plot((set.test[,1]* trainParam$std[1]) + trainParam$mean[1],(predict_glm* trainParam$std[1]) + trainParam$mean[1])
